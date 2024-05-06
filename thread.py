bf02f575b5b49213c6521f0c78a47ebf94740dfc
sachin <sachin.aralapura@gmail.com>
2024-05-06 18:03:57 +0530

    organized the code

--- thread.py
+++ thread.py
@@ -1,26 +1,4 @@
-import cv2
-
-ageProto = "models/age_deploy.prototxt"
-ageModel = "models/age_net.caffemodel"
-genderProto = "models/gender_deploy.prototxt"
-genderModel = "models/gender_net.caffemodel"
-MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-
-
-gender_list = ["male", "female"]
-ageList = [
-    "(0-5)",
-    "(5-15)",
-    "(15-20)",
-    "(21-30)",
-    "(30-40)",
-    "(40-55)",
-    "(56-70)",
-    "(70-100)",
-]
-
-ageNet = cv2.dnn.readNet(ageModel, ageProto)
-genderNet = cv2.dnn.readNet(genderModel, genderProto)
+from models.initialize_model import *
 
 
 def process_image(result, frame, faces):
